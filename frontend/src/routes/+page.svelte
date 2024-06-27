850cdea72c10a59bc50c2e67e0c89d5994a343fd
justusjb <jujo03h@icloud.com>
2024-06-26 23:03:37 -0700

    Removed redundant code

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,38 +1,7 @@
 <script lang="ts">
     export let data: { hackathons: { name: string; date: { start_date: string; end_date: string };  location: {city: string; country:string}; URL: string; status: string}[] };
     import { HackathonCard } from '../lib';
-
-    // Function to format the date
-    function formatDate(dateString: string): string {
-        const date = new Date(dateString);
-        return date.toLocaleDateString();
-    }
 </script>
-
-
-
-<style>
-    table {
-        /* width: 100%; */
-        border-collapse: collapse;
-        margin: 20px 0;
-        font-size: 18px;
-        text-align: left;
-    }
-
-    th, td {
-        padding: 12px;
-        border-bottom: 1px solid #ddd;
-    }
-
-    th {
-        background-color: #f2f2f2;
-    }
-
-    tr:hover {
-        background-color: #f5f5f5;
-    }
-</style>
 
 
 <svelte:head>
